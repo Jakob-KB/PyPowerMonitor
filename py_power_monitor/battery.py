6fa56b2ac330bb493b807988e82370e800a30b4f
Jakob-KB <jakob.kb@outlook.com>
2025-03-29 12:10:14 +1000

    feat: implementing battery monitoring with ctypes

---
+++ py_power_monitor/battery.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+
+import ctypes
+from ctypes import c_byte, c_ulong, Structure
+
+
+class SYSTEM_POWER_STATUS(Structure):
+    """
+    Represents the power status of the system, as retrieved from the Windows API.
+
+    :parameter:
+
+    """
+    _fields_ = [
+        ("ACLineStatus", c_byte),  # Power connection status flags
+        ("BatteryFlag", c_byte),  # Battery status flags
+        ("BatteryLifePercent", c_byte),  # Battery percentage
+        ("SystemStatusFlag", c_byte),  # Reserved
+        ("BatteryLifeTime", c_ulong),  # Seconds of battery life remaining
+        ("BatteryFullLifeTime", c_ulong)  # Seconds of full battery life
+    ]
+
+
+def get_system_power_status() -> SYSTEM_POWER_STATUS:
+    """
+    Retrieves the current power status of the system using the Windows API.
+
+    :rtype: SYSTEM_POWER_STATUS
+    :returns:
+        An instance of SYSTEM_POWER_STATUS containing the current system power information.
+    :raises Exception:
+        If the retrieval of the system power status fails.
+    """
+    # Call the GetSystemPowerStatus function from kernel32.dll
+    status = SYSTEM_POWER_STATUS()
+    result = ctypes.windll.kernel32.GetSystemPowerStatus(ctypes.byref(status))
+
+    # Check if the result is valid, if not raise an exception
+    if result:
+        return status
+    else:
+        raise Exception("Failed to get system power status")
+        # Could instead: raise ctypes.WinError()
+
+
+def main():
+    """
+    Example Usage.
+    """
+    try:
+        curr_status = get_system_power_status()
+        print("AC Line Status:", curr_status.ACLineStatus)
+        print("Battery Flag:", curr_status.BatteryFlag)
+        print("Battery Life Percent:", curr_status.BatteryLifePercent, "%")
+        print("Battery Life Time:", curr_status.BatteryLifeTime, "seconds")
+        print("Battery Full Life Time:", curr_status.BatteryFullLifeTime, "seconds")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
